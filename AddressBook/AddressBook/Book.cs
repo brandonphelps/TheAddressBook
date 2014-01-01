ed16e87a4f0811e8fd0efeba3409307bf5515673
Brandon Phelps <bwp44f@mst.edu>
2014-01-01 10:48:22 -0600

    added more functionality to book class such as getContacts

--- AddressBook/AddressBook/Book.cs
+++ AddressBook/AddressBook/Book.cs
@@ -9,15 +9,26 @@ namespace AddressBook
     class Book
     {
         private List<Contact> contacts;
-
-        public Book()
+        private string name;
+        public Book(string name)
         {
             contacts = new List<Contact>();
+            this.name = name;
         }
 
         public void addPerson(Contact c)
         {
             contacts.Add(c);
+        }
+        
+        public List<Contact> getContacts()
+        {
+          return contacts;
+        }
+
+        public override string ToString()
+        {
+            return name;
         }
     }
 }
