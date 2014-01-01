51cf1ec0bc8a0b6a3fdb0565fe88a035a8b71561
Brandon Phelps <bwp44f@mst.edu>
2014-01-01 10:48:55 -0600

    added toString and constructor to Contact class

--- AddressBook/AddressBook/Contact.cs
+++ AddressBook/AddressBook/Contact.cs
@@ -8,10 +8,19 @@ namespace AddressBook
 {
     class Contact
     {
+      public Contact(string c)
+      {
+        name = c;
+      }
+
       public string name { get; private set; }
       public string phone { get; private set; }
       public string emailAddress { get; private set; }
       public string streetAddress { get; private set; }
       public DateTime birthday { get; private set; }
+      public override string ToString()
+      {
+        return name;
+      }
     }
 }
